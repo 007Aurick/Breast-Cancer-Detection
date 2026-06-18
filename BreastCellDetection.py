f3f045cdbde56b029ab0348016211439916bda7a
Aurick Anwar <aurick.anwar2260biga@gmail.com>
2026-06-18 13:48:49 -0400

    Installed gradcam

--- BreastCellDetection.py
+++ BreastCellDetection.py
@@ -1,13 +1,7 @@
-#from torchvision import models
 from ultralytics import YOLO
-#from pytorch_grad_cam import GradCAM
-#rom pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-#from pytorch_grad_cam.utils.image import show_cam_on_image
-#import torch
-#import torch.nn as nn
+from gradcam import GradCAM
 import cv2
-#import numpy as np
-#import pandas as pd
+
 
 
 img_path = "6_jpg.rf.b3dbdcd56ecce6980a163a59431dad5d.jpg"
@@ -19,11 +13,11 @@ cv2.imshow("YOLO", annotated_frame)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-#class CancerCNN(nn.Module):
- #   def __init__(self):
-  #  super().__init__()
-  #  self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-   # self.relu1 = nn.ReLU()
+class CancerCNN(nn.Module):
+   def __init__(self):
+      super().__init__()
+      
+ 
 
 
 #cnn_model = CancerCNN()
